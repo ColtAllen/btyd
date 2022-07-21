ee027c7b97d1621546e566213c7027c56e866ed6
Colt Allen <colt.allen@coffeeandbagels.com>
2022-07-21 22:08:31 +0000

    Ran black code formatter on models module

--- btyd/models/__init__.py
+++ btyd/models/__init__.py
@@ -19,6 +19,7 @@ from ..utils import _check_inputs
 
 SELF = TypeVar("SELF")
 
+
 class BaseModel(ABC, Generic[SELF]):
     """
     Abstract class defining all base model methods as well as methods to be overridden when creating a model subclass.
@@ -29,25 +30,25 @@ class BaseModel(ABC, Generic[SELF]):
 
     @abstractmethod
     def __init__(self) -> SELF:
-        """ self._param_list must be instantiated here, as well as model hyperpriors."""
+        """self._param_list must be instantiated here, as well as model hyperpriors."""
 
     @abstractmethod
     def _model(self) -> None:
-        """ pymc model defining priors for model parameters and wrapping _log_likelihood in Potential()."""
+        """pymc model defining priors for model parameters and wrapping _log_likelihood in Potential()."""
         pass
 
     @abstractmethod
     def _log_likelihood(self) -> None:
-        """ Log-likelihood function for randomly drawn individual from customer population. Must be constructed from aesara tensors. """
+        """Log-likelihood function for randomly drawn individual from customer population. Must be constructed from aesara tensors."""
         pass
 
     @abstractmethod
     def generate_rfm_data(self) -> None:
-        """ Generate synthetic RFM data from parameters of fitted model. This is useful for posterior predictive checks and hyperparameter optimization. """
+        """Generate synthetic RFM data from parameters of fitted model. This is useful for posterior predictive checks and hyperparameter optimization."""
         pass
 
     def __repr__(self) -> str:
-        """ String representation of BTYD model object."""
+        """String representation of BTYD model object."""
         classname = self.__class__.__name__
         try:
             row_str = f"estimated with {self._frequency.shape[0]} customers."
@@ -55,12 +56,12 @@ class BaseModel(ABC, Generic[SELF]):
             row_str = ""
 
         try:
-            param_vals = np.around(self._unload_params(),decimals=1)
+            param_vals = np.around(self._unload_params(), decimals=1)
             param_str = str(dict(zip(self._param_list, param_vals)))
             return f"<btyd.{classname}: Parameters {param_str} {row_str}>"
         except AttributeError:
             return f"<btyd.{classname}>"
-    
+
     def fit(self, rfm_df: pd.DataFrame, tune: int = 1200, draws: int = 1200) -> SELF:
         """
         Fit a custom pymc model with parameter prior definitions to observed RFM data.
@@ -73,7 +74,7 @@ class BaseModel(ABC, Generic[SELF]):
             Number of starting 'burn-in' samples for posterior parameter distribution convergence. These are discarded after the model is fit.
         draws: int
             Number of samples from posterior parameter distrutions after tune period. These are retained for model usage.
-        
+
         Returns
         -------
         self
@@ -81,31 +82,39 @@ class BaseModel(ABC, Generic[SELF]):
 
         """
 
-        self._frequency, self._recency, self._T, self._monetary_value, _ = self._dataframe_parser(rfm_df)
+        (
+            self._frequency,
+            self._recency,
+            self._T,
+            self._monetary_value,
+            _,
+        ) = self._dataframe_parser(rfm_df)
 
         with self._model():
             self._idata = pm.sample(
                 tune=tune,
                 draws=draws,
                 chains=4,
-                cores=4, 
+                cores=4,
                 target_accept=0.95,
-                return_inferencedata=True
+                return_inferencedata=True,
             )
-        
+
         return self
-    
-    def _unload_params(self, posterior: bool = False, n_samples: int = 100) -> Union[Tuple[np.ndarray],Tuple[np.ndarray]]:
+
+    def _unload_params(
+        self, posterior: bool = False, n_samples: int = 100
+    ) -> Union[Tuple[np.ndarray], Tuple[np.ndarray]]:
         """
         Extract parameter posteriors from _idata InferenceData attribute of fitted model.
         This is an internal method and not intended to be called directly.
-        
+
         Parameters
         ----------
         posterior: bool
             Flag for sampling from parameter posterior distribution. Default is set for point estimates.
         n_samples: int
-        
+
         Returns
         -------
         parameters: tuple(np.ndarray)
@@ -114,9 +123,9 @@ class BaseModel(ABC, Generic[SELF]):
         """
 
         # BETA TODO: Raise BTYDException.
-         # if dict(filter(lambda item: self.__class__.__name__ not in item[0], self._idata.posterior.get('data_vars').items()))
-             # raise BTYDException
-        
+        # if dict(filter(lambda item: self.__class__.__name__ not in item[0], self._idata.posterior.get('data_vars').items()))
+        # raise BTYDException
+
         # test param exception (need another saved model and additions to self._unload_params())
         # test prediction exception if attempting to run predictions on instantiated model without RFM data.
 
@@ -124,32 +133,38 @@ class BaseModel(ABC, Generic[SELF]):
             return tuple(
                 [
                     self._sample(
-                    self._idata.posterior.get(f'{self.__class__.__name__}::{var}').values.flatten(),
-                     n_samples) 
-                     for var in self._param_list
-                    ]
-                )
+                        self._idata.posterior.get(
+                            f"{self.__class__.__name__}::{var}"
+                        ).values.flatten(),
+                        n_samples,
+                    )
+                    for var in self._param_list
+                ]
+            )
 
         else:
             return tuple(
                 [
-                    self._idata.posterior.get(f'{self.__class__.__name__}::{var}').mean().to_numpy()
+                    self._idata.posterior.get(f"{self.__class__.__name__}::{var}")
+                    .mean()
+                    .to_numpy()
                     for var in self._param_list
-                    ]
-                )
+                ]
+            )
 
-    def predict(self, 
-        method:str,
-        t: int = None, 
-        n: int = None, 
-        sample_posterior: bool =  False,
+    def predict(
+        self,
+        method: str,
+        t: int = None,
+        n: int = None,
+        sample_posterior: bool = False,
         posterior_draws: int = 100,
         rfm_df: pd.DataFrame = None,
-        join_df = False 
-        ) -> np.ndarray:
+        join_df=False,
+    ) -> np.ndarray:
         """
         Base method for running model predictions.
-        
+
         Parameters
         ----------
         method: str
@@ -166,7 +181,7 @@ class BaseModel(ABC, Generic[SELF]):
             Dataframe containing recency, frequency, monetary value, and time period columns. Only required if model is loaded from an external file.
         join_df: bool
             NOT SUPPORTED IN 0.1beta1. Flag to add columns to rfm_df containing predictive outputs.
-        
+
         Returns
         -------
         predictions: np.ndarray
@@ -175,21 +190,29 @@ class BaseModel(ABC, Generic[SELF]):
         """
 
         if rfm_df is None:
-            self._frequency, self._recency, self._T, self._monetary_value, _ = self._dataframe_parser(rfm_df)
+            (
+                self._frequency,
+                self._recency,
+                self._T,
+                self._monetary_value,
+                _,
+            ) = self._dataframe_parser(rfm_df)
 
         # TODO: Add exception handling for method argument.
-        predictions = self._quantities_of_interest.get(method)(self,t,n,sample_posterior,posterior_draws)
+        predictions = self._quantities_of_interest.get(method)(
+            self, t, n, sample_posterior, posterior_draws
+        )
 
         # TODO: Add arg to automatically merge to RFM dataframe?
         if join_df:
             pass
-        
+
         if sample_posterior:
             # Additional columns will need to be added for mean, confidence intervals, etc.
             pass
-        
+
         return predictions
-    
+
     def save(self, filename: str) -> None:
         """
         Dump InferenceData from fitted model into a JSON or CSV file. Format is inferred from the filename.
@@ -200,11 +223,11 @@ class BaseModel(ABC, Generic[SELF]):
             Path and/or filename where model data will be saved.
         """
 
-        if '.json' in filename:
+        if ".json" in filename:
             self._idata.to_json(filename)
-        elif '.csv' in filename:
-            df = self._idata.to_dataframe()#include_index=False)
-            df.to_csv(filename)#,index=False)
+        elif ".csv" in filename:
+            df = self._idata.to_dataframe()  # include_index=False)
+            df.to_csv(filename)  # ,index=False)
 
     def load(self, filename: str) -> SELF:
         """
@@ -221,31 +244,31 @@ class BaseModel(ABC, Generic[SELF]):
             Model object containing ``_idata`` attribute for model evaluation and predictions.
         """
 
-        if '.json' in filename:
+        if ".json" in filename:
             self._idata = az.from_json(filename)
         # TODO: CSV loadings currently encountering issues.
         # elif '.csv' in filename:
         #     df = pd.read_csv(filename).to_dict()
         #     self._idata = az.from_dict(df)
-            # xdata = xr.Dataset.from_dataframe(df)
-            # self._idata = az.convert_to_inference_data(xdata)
+        # xdata = xr.Dataset.from_dataframe(df)
+        # self._idata = az.convert_to_inference_data(xdata)
 
         # BETA TODO: Raise BTYDException.
         # if dict(filter(lambda item: self.__class__.__name__ not in item[0], self._idata.posterior.get('data_vars').items()))
-            # raise BTYDException
+        # raise BTYDException
 
         return self
-        
+
     @staticmethod
     def _dataframe_parser(rfm_df: pd.DataFrame) -> Tuple[np.ndarray]:
-        """ 
+        """
         Parse input dataframe into separate RFM components. This is an internal method and not intended to be called directly.
 
         Parameters
         ----------
         rfm_df: pandas.DataFrame
             Dataframe containing recency, frequency, monetary value, and time period columns.
-        
+
         Returns
         -------
             Tuple containing numpy arrays for Recency, Frequency, Monetary Value, T, and Customer ID (if provided).
@@ -254,15 +277,15 @@ class BaseModel(ABC, Generic[SELF]):
         rfm_df.columns = rfm_df.columns.str.upper()
 
         # The load_cdnow_summary_with_monetary_value() function needs an ID column for testing.
-        if 'ID' not in rfm_df.columns:
+        if "ID" not in rfm_df.columns:
             customer = rfm_df.index.values
         else:
-            customer = rfm_df['ID'].values
-        
-        frequency = rfm_df['FREQUENCY'].values
-        recency = rfm_df['RECENCY'].values
-        T = rfm_df['T'].values
-        monetary_value = rfm_df['MONETARY_VALUE'].values
+            customer = rfm_df["ID"].values
+
+        frequency = rfm_df["FREQUENCY"].values
+        recency = rfm_df["RECENCY"].values
+        T = rfm_df["T"].values
+        monetary_value = rfm_df["MONETARY_VALUE"].values
 
         # TODO: Add monetary_value to this, and consider ID continengent on predict() outputs.
         _check_inputs(frequency, recency, T)
@@ -273,7 +296,7 @@ class BaseModel(ABC, Generic[SELF]):
     def _sample(param_array: array_like, n_samples: int) -> np.ndarray:
         """
         Utility function for sampling from parameter posteriors. This is an internal method and not intended to be called directly.
-        
+
         Parameters
         ----------
         param_array: array_like
@@ -300,48 +323,48 @@ class PredictMixin(ABC, Generic[SELF]):
 
     @abstractmethod
     def _conditional_probability_alive(
-        self, 
-        t: float = None, 
-        n: int = None, 
+        self,
+        t: float = None,
+        n: int = None,
         sample_posterior: bool = False,
-        posterior_draws: int = 100
-        ) -> None:
+        posterior_draws: int = 100,
+    ) -> None:
         pass
-    
+
     @abstractmethod
     def _conditional_expected_number_of_purchases_up_to_time(
-        self, 
-        t: float = None, 
-        n: int = None, 
+        self,
+        t: float = None,
+        n: int = None,
         sample_posterior: bool = False,
-        posterior_draws: int = 100
-        ) -> None:
+        posterior_draws: int = 100,
+    ) -> None:
         pass
-    
+
     @abstractmethod
     def _expected_number_of_purchases_up_to_time(
-        self, 
-        t: float = None, 
-        n: int = None, 
+        self,
+        t: float = None,
+        n: int = None,
         sample_posterior: bool = False,
-        posterior_draws: int = 100
-        ) -> None:
+        posterior_draws: int = 100,
+    ) -> None:
         pass
-    
+
     @abstractmethod
     def _probability_of_n_purchases_up_to_time(
-        self, 
-        t: float = None, 
-        n: int = None, 
+        self,
+        t: float = None,
+        n: int = None,
         sample_posterior: bool = False,
-        posterior_draws: int = 100
-        ) -> None:
+        posterior_draws: int = 100,
+    ) -> None:
         pass
-    
+
     # BETA TODO: this attribute must be re-declared in model subclasses and is retained here for documentation purposes only.
     _quantities_of_interest = {
-        'cond_prob_alive': _conditional_probability_alive,
-        'cond_n_prchs_to_time': _conditional_expected_number_of_purchases_up_to_time,
-        'n_prchs_to_time': _expected_number_of_purchases_up_to_time,
-        'prob_n_prchs_to_time': _probability_of_n_purchases_up_to_time,
-        }
+        "cond_prob_alive": _conditional_probability_alive,
+        "cond_n_prchs_to_time": _conditional_expected_number_of_purchases_up_to_time,
+        "n_prchs_to_time": _expected_number_of_purchases_up_to_time,
+        "prob_n_prchs_to_time": _probability_of_n_purchases_up_to_time,
+    }
