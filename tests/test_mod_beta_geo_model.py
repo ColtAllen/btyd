b6030481441ef68cdd2ccaf723274a7cba1b6e0f
ColtAllen <colt.allen1@gmail.com>
2022-10-19 20:59:25 +0000

    Initial commit for ModBetaGeoModel

---
+++ tests/test_mod_beta_geo_model.py
@@ -0,0 +1,19 @@
+from __future__ import generator_stop
+from __future__ import annotations
+
+import os
+
+import pytest
+
+import pandas as pd
+import numpy as np
+import arviz as az
+
+import pymc as pm
+import aesara.tensor as at
+
+import btyd
+
+
+class TestModBetaGeoModel:
+    pass
